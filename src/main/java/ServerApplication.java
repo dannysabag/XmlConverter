4b6ac3d90c5b6eea93e265fd50ce34206e510daf
dannysabag <dannysabag@gmail.com>
2018-03-04 22:35:45 +0200

    for erez

--- src/main/java/ServerApplication.java
+++ src/main/java/ServerApplication.java
@@ -1,16 +1,23 @@
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @EnableAutoConfiguration
 @SpringBootConfiguration
 @Configuration
 public class ServerApplication {
 
     public static void main(String[] args) {
+        log.info("Starting XML Converter, at: {}", LocalDateTime.now());
         SpringApplication.run(ServerApplication.class, args);
         System.out.println("testing...");
     }
+
+    //  git push --set-upstream https://github.com/dannysabag/XmlConverter master
 
 }
